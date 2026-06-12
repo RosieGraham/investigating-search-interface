d6f64cdc26d1ef958a872842598c73498ef84d35
RosieGraham <rosienvgraham@gmail.com>
2026-06-12 12:28:48 +0100

    v3

--- web_extension_chrome/config.js
+++ web_extension_chrome/config.js
@@ -7,7 +7,7 @@ After deploying the backend, change API_BASE to the Render URL, e.g.
 */
 
 const ISI_CONFIG = {
-  API_BASE: 'http://localhost:8000',
+  API_BASE: 'https://investigating-search-interface.onrender.com',
   PROJECT_URL: 'https://github.com/bear-rsg/ethical-interface',
   ATTRIBUTION: 'Investigating Search Interface · SEEED',
   MAX_PROMPTS: 3,
